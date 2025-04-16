0c6090066d63efb93e2f89f9a004fc1c0024a0a8
Yewon316 <readchoi@naver.com>
2025-04-16 18:03:53 -0400

    API

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,  include
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('orders/', include('demos.urls')),
+    path('admin/', admin.site.urls),
+    path('orders/', include('orders.urls')),  
+
 ]
